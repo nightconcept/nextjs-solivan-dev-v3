40b2e60fd8c0690dfb7d5ea93f3d7166b5b4d751
Danny Solivan <dark@nightconcept.net>
2025-04-03 20:30:57 -0700

    chore: add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,18 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
 	title: '/home/danny',
-	description: 'Personal blog and website',
-	generator: 'v0.dev'
+	description: 'Personal blog and website'
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" suppressHydrationWarning>
+			<head>
+				<link rel="icon" href="/favicon.ico" sizes="any" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+			</head>
 			<body className={inter.className}>
 				<ThemeProvider
 					attribute="class"
