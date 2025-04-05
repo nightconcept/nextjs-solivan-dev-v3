6d17ada3746dbd6cbecdb139ccd562db6b31c826
Danny Solivan <dark@nightconcept.net>
2025-04-04 21:45:31 -0700

    chore: Update for release maybe

--- src/components/profile-card.tsx
+++ src/components/profile-card.tsx
@@ -5,8 +5,14 @@ export default function ProfileCard() {
     <div className="p-8">
       <h1 className="mb-4 text-3xl font-bold">Hi, I&apos;m Danny</h1>
       <p className="text-muted-foreground dark:text-muted-foreground mb-6 text-lg">
-        I&apos;m an engineer by day, gamer, developer, writer, poet, and Stoic
-        by night.
+        I&rsquo;m an engineer by day, gamer, developer, writer, poet, and Stoic
+        by night. I like thinking and growing my thoughts continuosly. This site
+        serves as my place where I 📚{" "}
+        <a href="https://www.swyx.io/learn-in-public">learn in public</a>{" "}
+        and grow my 💻🌱{" "}
+        <a href="https://maggieappleton.com/garden-history">digital garden</a>,
+        but more accurately a simple{" "}
+        <a href="https://www.ryrob.com/history-of-blogging/">blog</a>.
       </p>
       <SocialLinks spacing="normal" />
     </div>
